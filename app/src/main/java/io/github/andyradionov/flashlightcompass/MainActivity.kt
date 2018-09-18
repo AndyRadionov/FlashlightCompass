d8ff83108a289e3591a280564ad3f7e8d5333b06
Andrey Radionov <andyomsk@gmail.com>
2018-09-18 23:40:03 +0300

    FirstFlashLightVersion

--- app/src/main/java/io/github/andyradionov/flashlightcompass/MainActivity.kt
+++ app/src/main/java/io/github/andyradionov/flashlightcompass/MainActivity.kt
@@ -1,12 +1,97 @@
 package io.github.andyradionov.flashlightcompass
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var flashLightStatus = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        initListeners()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                buttonEnable.isEnabled = false
+                buttonEnable.text = "Camera Enabled"
+                imageFlashlight.isEnabled = true
+            } else {
+                Toast.makeText(this@MainActivity, "Permission Denied for the Camera",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initViews() {
+        val isEnabled = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED
+
+        buttonEnable.isEnabled = !isEnabled;
+        imageFlashlight.isEnabled = isEnabled;
+    }
+
+    private fun initListeners() {
+        val hasCameraFlash = packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+
+        buttonEnable.setOnClickListener {
+            ActivityCompat.requestPermissions(MainActivity@this,
+                    arrayOf(Manifest.permission.CAMERA), Companion.CAMERA_REQUEST);
+        }
+
+        imageFlashlight.setOnClickListener {
+            if (hasCameraFlash) {
+                if (flashLightStatus)
+                    flashLightOff();
+                else
+                    flashLightOn();
+            } else {
+                Toast.makeText(MainActivity@this, "No flash available on your device",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private fun flashLightOn() {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        try {
+            val cameraId = cameraManager.cameraIdList[0]
+            cameraManager.setTorchMode(cameraId, true)
+            flashLightStatus = true
+            imageFlashlight.setImageResource(R.drawable.btn_switch_on)
+        } catch (e: CameraAccessException) {
+        }
+    }
+
+    private fun flashLightOff() {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        try {
+            val cameraId = cameraManager.cameraIdList[0]
+            cameraManager.setTorchMode(cameraId, false)
+            flashLightStatus = false
+            imageFlashlight.setImageResource(R.drawable.btn_switch_off)
+        } catch (e: CameraAccessException) {
+        }
+
+    }
+
+    companion object {
+        private const val CAMERA_REQUEST = 50
     }
 }
